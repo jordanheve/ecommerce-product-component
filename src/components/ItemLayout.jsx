098eb7fef0e6a06df6fcbe01dc5dc63047118d83
Jordan Herrera Vera <jordan.herreravera@gmail.com>
2023-11-01 17:25:31 -0600

    updated price component location

--- src/components/ItemLayout.jsx
+++ src/components/ItemLayout.jsx
@@ -1,12 +1,11 @@
 import ItemSlider from './ItemSlider'
-import ItemPrice from './itemPrice'
+import ItemPrice from './ItemPrice'
 
 export default function ItemLayout() {
 
 
   return (
     <main className='flex max-md:flex-col  items-center justify-evenly h-[calc(100%-5rem)]'>
-        
         <ItemSlider/>
         <ItemPrice />
     </main>
